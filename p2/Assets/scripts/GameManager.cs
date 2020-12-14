e0d6b5cdae7c1c051ff56db5703c66a8aa34c996
Rubem Fridolino Christ Neto <rubemchrist@gmail.com>
2020-12-14 17:47:46 +0000

    created

---
+++ p2/Assets/scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform targetPlayer;
+    public LayerMask world;
+    //targetPlayer Movement Functions
+    public void Move_UP()
+    {
+        if(Physics2D.OverlapCircle(targetPlayer.position+new Vector3(0,1,0),0.1f,world))
+        {
+            targetPlayer.position = targetPlayer.position+new Vector3(0,1,0);
+        }
+    }
+    public void Move_DOWN()
+    {
+        if(Physics2D.OverlapCircle(targetPlayer.position+new Vector3(0,-1,0),0.1f,world))
+        {
+            targetPlayer.position = targetPlayer.position+new Vector3(0,-1,0);
+        }
+    }
+    public void Move_LEFT()
+    {
+        if(Physics2D.OverlapCircle(targetPlayer.position+new Vector3(-1,0,0),0.1f,world))
+        {
+            targetPlayer.position = targetPlayer.position+new Vector3(-1,0,0); 
+        }
+    }
+    public void Move_RIGHT()
+    {
+        if(Physics2D.OverlapCircle(targetPlayer.position+new Vector3(1,0,0),0.1f,world))
+        {
+            targetPlayer.position = targetPlayer.position+new Vector3(1,0,0);
+        }
+    }
+    public void ROTATE()
+    {
+
+    }
+    public void GRAB()
+    {
+
+    }
+}
---
+++ p2/Assets/scripts/player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public Transform target;
+    public float movementSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target.SetParent(null);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,target.position,movementSpeed*Time.deltaTime);
+    }
+}
